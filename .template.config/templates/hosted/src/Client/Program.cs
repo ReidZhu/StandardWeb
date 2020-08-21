3ad95de30b7390495572f976f1f12da353727f85
ReidZhu <1197561976@qq.com>
2020-08-21 10:50:08 +0800

    Initial commit

---
+++ .template.config/templates/hosted/src/Client/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AntDesign.Pro.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddAntDesign();
+            await builder.Build().RunAsync();
+        }
+    }
+}
